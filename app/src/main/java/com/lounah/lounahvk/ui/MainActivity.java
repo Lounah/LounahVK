2b458ba274ee750b2368780eab8a2d186fc93cdc
Lounah <30981413+Lounah@users.noreply.github.com>
2018-05-01 14:46:37 +0300

    some updates

---
+++ app/src/main/java/com/lounah/lounahvk/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lounah.lounahvk.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lounah.lounahvk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
